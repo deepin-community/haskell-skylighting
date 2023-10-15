e0d1c18e4068f93686ddf02aeec070d845597f31
bluesky <chenchongbiao@deepin.org>
2023-10-15 22:16:43 +0800

    chore: update repo
    update version to 0.12.3.1-1
    
    Issue: https://github.com/linuxdeepin/developer-center/issues/5731
    Log: update repo

--- src/Skylighting/Syntax.hs
+++ src/Skylighting/Syntax.hs
@@ -9,7 +9,6 @@ import qualified Skylighting.Syntax.Actionscript
 import qualified Skylighting.Syntax.Ada
 import qualified Skylighting.Syntax.Agda
 import qualified Skylighting.Syntax.Alert
-import qualified Skylighting.Syntax.AlertIndent
 import qualified Skylighting.Syntax.Apache
 import qualified Skylighting.Syntax.Asn1
 import qualified Skylighting.Syntax.Asp
@@ -24,6 +23,7 @@ import qualified Skylighting.Syntax.Clojure
 import qualified Skylighting.Syntax.Cmake
 import qualified Skylighting.Syntax.Coffee
 import qualified Skylighting.Syntax.Coldfusion
+import qualified Skylighting.Syntax.Comments
 import qualified Skylighting.Syntax.Commonlisp
 import qualified Skylighting.Syntax.Cpp
 import qualified Skylighting.Syntax.Cs
@@ -44,12 +44,15 @@ import qualified Skylighting.Syntax.Elm
 import qualified Skylighting.Syntax.Email
 import qualified Skylighting.Syntax.Erlang
 import qualified Skylighting.Syntax.Fasm
-import qualified Skylighting.Syntax.Fortran
+import qualified Skylighting.Syntax.FortranFixed
+import qualified Skylighting.Syntax.FortranFree
 import qualified Skylighting.Syntax.Fsharp
 import qualified Skylighting.Syntax.Gcc
 import qualified Skylighting.Syntax.Glsl
 import qualified Skylighting.Syntax.Gnuassembler
 import qualified Skylighting.Syntax.Go
+import qualified Skylighting.Syntax.Graphql
+import qualified Skylighting.Syntax.Groovy
 import qualified Skylighting.Syntax.Hamlet
 import qualified Skylighting.Syntax.Haskell
 import qualified Skylighting.Syntax.Haxe
@@ -60,8 +63,8 @@ import qualified Skylighting.Syntax.Isocpp
 import qualified Skylighting.Syntax.J
 import qualified Skylighting.Syntax.Java
 import qualified Skylighting.Syntax.Javadoc
-import qualified Skylighting.Syntax.JavascriptReact
 import qualified Skylighting.Syntax.Javascript
+import qualified Skylighting.Syntax.JavascriptReact
 import qualified Skylighting.Syntax.Json
 import qualified Skylighting.Syntax.Jsp
 import qualified Skylighting.Syntax.Julia
@@ -89,12 +92,15 @@ import qualified Skylighting.Syntax.Modula3
 import qualified Skylighting.Syntax.Monobasic
 import qualified Skylighting.Syntax.Mustache
 import qualified Skylighting.Syntax.Nasm
+import qualified Skylighting.Syntax.Nim
+import qualified Skylighting.Syntax.Nix
 import qualified Skylighting.Syntax.Noweb
 import qualified Skylighting.Syntax.Objectivec
 import qualified Skylighting.Syntax.Objectivecpp
 import qualified Skylighting.Syntax.Ocaml
 import qualified Skylighting.Syntax.Octave
 import qualified Skylighting.Syntax.Opencl
+import qualified Skylighting.Syntax.Orgmode
 import qualified Skylighting.Syntax.Pascal
 import qualified Skylighting.Syntax.Perl
 import qualified Skylighting.Syntax.Php
@@ -109,6 +115,7 @@ import qualified Skylighting.Syntax.Purebasic
 import qualified Skylighting.Syntax.Python
 import qualified Skylighting.Syntax.Qml
 import qualified Skylighting.Syntax.R
+import qualified Skylighting.Syntax.Raku
 import qualified Skylighting.Syntax.Relaxng
 import qualified Skylighting.Syntax.Relaxngcompact
 import qualified Skylighting.Syntax.Rest
@@ -116,19 +123,26 @@ import qualified Skylighting.Syntax.Rhtml
 import qualified Skylighting.Syntax.Roff
 import qualified Skylighting.Syntax.Ruby
 import qualified Skylighting.Syntax.Rust
+import qualified Skylighting.Syntax.Sass
 import qualified Skylighting.Syntax.Scala
 import qualified Skylighting.Syntax.Scheme
 import qualified Skylighting.Syntax.Sci
+import qualified Skylighting.Syntax.Scss
 import qualified Skylighting.Syntax.Sed
 import qualified Skylighting.Syntax.Sgml
 import qualified Skylighting.Syntax.Sml
+import qualified Skylighting.Syntax.SpdxComments
+import qualified Skylighting.Syntax.Sql
 import qualified Skylighting.Syntax.SqlMysql
 import qualified Skylighting.Syntax.SqlPostgresql
-import qualified Skylighting.Syntax.Sql
+import qualified Skylighting.Syntax.Stan
 import qualified Skylighting.Syntax.Stata
+import qualified Skylighting.Syntax.Swift
+import qualified Skylighting.Syntax.Systemverilog
 import qualified Skylighting.Syntax.Tcl
 import qualified Skylighting.Syntax.Tcsh
 import qualified Skylighting.Syntax.Texinfo
+import qualified Skylighting.Syntax.Toml
 import qualified Skylighting.Syntax.Typescript
 import qualified Skylighting.Syntax.Verilog
 import qualified Skylighting.Syntax.Vhdl
@@ -148,7 +162,6 @@ defaultSyntaxMap = Map.fromList [
   ,  ("Ada", Skylighting.Syntax.Ada.syntax)
   ,  ("Agda", Skylighting.Syntax.Agda.syntax)
   ,  ("Alerts", Skylighting.Syntax.Alert.syntax)
-  ,  ("Alerts_indent", Skylighting.Syntax.AlertIndent.syntax)
   ,  ("Apache Configuration", Skylighting.Syntax.Apache.syntax)
   ,  ("ASN.1", Skylighting.Syntax.Asn1.syntax)
   ,  ("ASP", Skylighting.Syntax.Asp.syntax)
@@ -163,6 +176,7 @@ defaultSyntaxMap = Map.fromList [
   ,  ("CMake", Skylighting.Syntax.Cmake.syntax)
   ,  ("CoffeeScript", Skylighting.Syntax.Coffee.syntax)
   ,  ("ColdFusion", Skylighting.Syntax.Coldfusion.syntax)
+  ,  ("Comments", Skylighting.Syntax.Comments.syntax)
   ,  ("Common Lisp", Skylighting.Syntax.Commonlisp.syntax)
   ,  ("C++", Skylighting.Syntax.Cpp.syntax)
   ,  ("C#", Skylighting.Syntax.Cs.syntax)
@@ -183,12 +197,15 @@ defaultSyntaxMap = Map.fromList [
   ,  ("Email", Skylighting.Syntax.Email.syntax)
   ,  ("Erlang", Skylighting.Syntax.Erlang.syntax)
   ,  ("Intel x86 (FASM)", Skylighting.Syntax.Fasm.syntax)
-  ,  ("Fortran", Skylighting.Syntax.Fortran.syntax)
+  ,  ("Fortran (Fixed Format)", Skylighting.Syntax.FortranFixed.syntax)
+  ,  ("Fortran (Free Format)", Skylighting.Syntax.FortranFree.syntax)
   ,  ("FSharp", Skylighting.Syntax.Fsharp.syntax)
   ,  ("GCCExtensions", Skylighting.Syntax.Gcc.syntax)
   ,  ("GLSL", Skylighting.Syntax.Glsl.syntax)
   ,  ("GNU Assembler", Skylighting.Syntax.Gnuassembler.syntax)
   ,  ("Go", Skylighting.Syntax.Go.syntax)
+  ,  ("GraphQL", Skylighting.Syntax.Graphql.syntax)
+  ,  ("Groovy", Skylighting.Syntax.Groovy.syntax)
   ,  ("Hamlet", Skylighting.Syntax.Hamlet.syntax)
   ,  ("Haskell", Skylighting.Syntax.Haskell.syntax)
   ,  ("Haxe", Skylighting.Syntax.Haxe.syntax)
@@ -199,8 +216,8 @@ defaultSyntaxMap = Map.fromList [
   ,  ("J", Skylighting.Syntax.J.syntax)
   ,  ("Java", Skylighting.Syntax.Java.syntax)
   ,  ("Javadoc", Skylighting.Syntax.Javadoc.syntax)
-  ,  ("JavaScript React (JSX)", Skylighting.Syntax.JavascriptReact.syntax)
   ,  ("JavaScript", Skylighting.Syntax.Javascript.syntax)
+  ,  ("JavaScript React (JSX)", Skylighting.Syntax.JavascriptReact.syntax)
   ,  ("JSON", Skylighting.Syntax.Json.syntax)
   ,  ("JSP", Skylighting.Syntax.Jsp.syntax)
   ,  ("Julia", Skylighting.Syntax.Julia.syntax)
@@ -228,12 +245,15 @@ defaultSyntaxMap = Map.fromList [
   ,  ("MonoBasic", Skylighting.Syntax.Monobasic.syntax)
   ,  ("Mustache/Handlebars (HTML)", Skylighting.Syntax.Mustache.syntax)
   ,  ("Intel x86 (NASM)", Skylighting.Syntax.Nasm.syntax)
+  ,  ("Nim", Skylighting.Syntax.Nim.syntax)
+  ,  ("Nix", Skylighting.Syntax.Nix.syntax)
   ,  ("noweb", Skylighting.Syntax.Noweb.syntax)
   ,  ("Objective-C", Skylighting.Syntax.Objectivec.syntax)
   ,  ("Objective-C++", Skylighting.Syntax.Objectivecpp.syntax)
   ,  ("Objective Caml", Skylighting.Syntax.Ocaml.syntax)
   ,  ("Octave", Skylighting.Syntax.Octave.syntax)
   ,  ("OpenCL", Skylighting.Syntax.Opencl.syntax)
+  ,  ("Org Mode", Skylighting.Syntax.Orgmode.syntax)
   ,  ("Pascal", Skylighting.Syntax.Pascal.syntax)
   ,  ("Perl", Skylighting.Syntax.Perl.syntax)
   ,  ("PHP/PHP", Skylighting.Syntax.Php.syntax)
@@ -248,6 +268,7 @@ defaultSyntaxMap = Map.fromList [
   ,  ("Python", Skylighting.Syntax.Python.syntax)
   ,  ("QML", Skylighting.Syntax.Qml.syntax)
   ,  ("R Script", Skylighting.Syntax.R.syntax)
+  ,  ("Raku", Skylighting.Syntax.Raku.syntax)
   ,  ("RELAX NG", Skylighting.Syntax.Relaxng.syntax)
   ,  ("RelaxNG-Compact", Skylighting.Syntax.Relaxngcompact.syntax)
   ,  ("reStructuredText", Skylighting.Syntax.Rest.syntax)
@@ -255,19 +276,26 @@ defaultSyntaxMap = Map.fromList [
   ,  ("Roff", Skylighting.Syntax.Roff.syntax)
   ,  ("Ruby", Skylighting.Syntax.Ruby.syntax)
   ,  ("Rust", Skylighting.Syntax.Rust.syntax)
+  ,  ("SASS", Skylighting.Syntax.Sass.syntax)
   ,  ("Scala", Skylighting.Syntax.Scala.syntax)
   ,  ("Scheme", Skylighting.Syntax.Scheme.syntax)
   ,  ("scilab", Skylighting.Syntax.Sci.syntax)
+  ,  ("SCSS", Skylighting.Syntax.Scss.syntax)
   ,  ("sed", Skylighting.Syntax.Sed.syntax)
   ,  ("SGML", Skylighting.Syntax.Sgml.syntax)
   ,  ("SML", Skylighting.Syntax.Sml.syntax)
+  ,  ("SPDX-Comments", Skylighting.Syntax.SpdxComments.syntax)
+  ,  ("SQL", Skylighting.Syntax.Sql.syntax)
   ,  ("SQL (MySQL)", Skylighting.Syntax.SqlMysql.syntax)
   ,  ("SQL (PostgreSQL)", Skylighting.Syntax.SqlPostgresql.syntax)
-  ,  ("SQL", Skylighting.Syntax.Sql.syntax)
+  ,  ("Stan", Skylighting.Syntax.Stan.syntax)
   ,  ("Stata", Skylighting.Syntax.Stata.syntax)
+  ,  ("Swift", Skylighting.Syntax.Swift.syntax)
+  ,  ("SystemVerilog", Skylighting.Syntax.Systemverilog.syntax)
   ,  ("Tcl/Tk", Skylighting.Syntax.Tcl.syntax)
   ,  ("Tcsh", Skylighting.Syntax.Tcsh.syntax)
   ,  ("Texinfo", Skylighting.Syntax.Texinfo.syntax)
+  ,  ("TOML", Skylighting.Syntax.Toml.syntax)
   ,  ("TypeScript", Skylighting.Syntax.Typescript.syntax)
   ,  ("Verilog", Skylighting.Syntax.Verilog.syntax)
   ,  ("VHDL", Skylighting.Syntax.Vhdl.syntax)
